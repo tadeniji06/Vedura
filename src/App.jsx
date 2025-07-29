fbb6cc5ced16e13adf3994b7d0bf006a7251d4ca
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-07-29 15:42:45 +0100

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import AppRoutes from "./routes/routes";
 import AnalyticsScript from "./components/AnalyticsScript";
 
 export default function App() {
-	const location = useLocation();
-
-	useEffect(() => {
-		// Delay execution just slightly in case gtag is still initializing
-		const timeout = setTimeout(() => {
-			if (window.gtag) {
-				window.gtag("config", "G-EE3EV4FPS7", {
-					page_path: location.pathname + location.search,
-				});
-			}
-		}, 300); // 300ms
-
-		return () => clearTimeout(timeout);
-	}, [location]);
-
 	return (
 		<>
 			<AnalyticsScript />
